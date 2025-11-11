db2bffd2db494cea463bdd3d70b349308c89cb5d
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-11 18:48:54 +0330

    feat: update MessagesController with CRUD operations for message handling

--- 03/src/messages/messages.controller.ts
+++ 03/src/messages/messages.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Patch } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+    @Get()
+    findAll(){
+        return "come and get all messages"
+    }
+
+    @Post()
+    create(){
+        return 'This action adds a new message';
+    }
+
+    @Delete()
+    remove(){
+        return 'This action removes a message';
+    }
+
+    @Patch()
+    update(){
+        return 'This action updates a message';
+    }
+
+    @Get(':id')
+    findOne(){
+        return 'This action returns a message';
+    }
+
+    }
