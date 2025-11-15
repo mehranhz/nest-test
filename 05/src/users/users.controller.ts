2bafb369592209e5d7956471bf6f3fe45588cde9
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-15 22:43:21 +0330

    add user service mthods

--- 05/src/users/users.controller.ts
+++ 05/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dtos/create-user.dto';
 
@@ -10,4 +10,15 @@ export class UsersController {
     createUser(@Body() body: CreateUserDTO){
         return this.usersService.create(body.email, body.password);
     }
+
+    @Get(':id')
+    findUser(@Param('id') id:string){
+        return this.usersService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email:string){
+        return this.usersService.find(email)
+    }
+
 }
