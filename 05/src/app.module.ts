a70ce2a21d0d89a581e7291334ed35ebbdd97c8c
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-15 16:59:13 +0330

    feat: implement user registration and report management features
    - Removed README.md as it is no longer needed.
    - Added database.sqlite for SQL.js storage.
    - Created User and Report entities with respective properties.
    - Implemented UsersController for user signup with validation.
    - Developed UsersService to handle user creation logic.
    - Introduced CreateUserDTO for input validation.
    - Updated app.module.ts to include new entities and configurations.

--- 05/src/app.module.ts
+++ 05/src/app.module.ts
@@ -2,19 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import {TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+import { join } from 'path';
 
 @Module({
   imports: [ReportsModule, UsersModule,TypeOrmModule.forRoot({
     type: 'sqljs',
-    location: 'database',
-    entities: [],
-    synchronize: true
+    autoSave: true,
+    synchronize: true,
+    location: 'database.sqlite',   // <--- This is the output file
+    entities: [User, Report],
+    sqlJsConfig: {
+      locateFile: (fileName) => join(__dirname, '..', 'node_modules', 'sql.js', 'dist', fileName),
+    },
   })],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
