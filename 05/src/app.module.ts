daca9fbbbca4dffc827e4bafefb53475a46c9c35
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-15 13:52:54 +0330

    feat: add Reports and Users modules with controllers and services

--- 05/src/app.module.ts
+++ 05/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ReportsModule, UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
