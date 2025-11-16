083518d1c961041264c0ec597d433c7daaaf39a3
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-16 08:37:16 +0330

    implement update and delete methods of user service

--- 05/src/users/users.controller.ts
+++ 05/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query,Delete, Patch, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -12,13 +13,28 @@ export class UsersController {
     }
 
     @Get(':id')
-    findUser(@Param('id') id:string){
-        return this.usersService.findOne(parseInt(id))
+    async findUser(@Param('id') id:string){
+        const user = await this.usersService.findOne(parseInt(id))
+        if(!user){
+            throw new NotFoundException('user not found')
+        }
+
+        return user
     }
 
     @Get()
     findAllUsers(@Query('email') email:string){
         return this.usersService.find(email)
+    }
+
+    @Delete(':id')
+    removeUser(@Param('id') id: string){
+        return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string, @Body() body:UpdateUserDto){
+        return this.usersService.update(parseInt(id), body)
     }
 
 }
