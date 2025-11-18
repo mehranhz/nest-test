aca8781206e56d49bbe3e3be76a10d41308a922c
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-18 17:49:25 +0330

    feat: add AuthService and update UsersModule to include it

---
+++ 05/src/users/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common'
+import { UsersService } from './users.service'
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  signup(email: string, password: string) {}
+
+  signin() {}
+}
