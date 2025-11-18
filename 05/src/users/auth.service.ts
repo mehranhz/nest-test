487dee81d0d3df2f754a44aa0f01e7de3bfde650
Mehran <hosseinzadehmehran4@gmail.com>
2025-11-18 18:01:48 +0330

    feat: implement signup method in AuthService with email validation

--- 05/src/users/auth.service.ts
+++ 05/src/users/auth.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { UsersService } from './users.service'
+import { randomBytes, scrypt as _scrypt } from 'crypto'
+import { promisify } from 'util'
+
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  signup(email: string, password: string) {}
+  async signup(email: string, password: string) {
+    const user = await this.usersService.find(email)
+    if (user.length) {
+      throw new BadRequestException('email in use')
+    }
+
+    const salt = randomBytes(8).toString('hex')
+  }
 
   signin() {}
 }
